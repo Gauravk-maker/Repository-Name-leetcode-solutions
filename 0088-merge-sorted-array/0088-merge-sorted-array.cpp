03bc3a8e2d034bd93593545e676d24c4c0832e3b
Gaurav Kumar <g75031620@gmail.com>
2026-08-25 01:43:47 +0530

    Time: 0 ms (100%), Space: 12.4 MB (76.03%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+
+    void swapIfGreater(vector<int>& a, vector<int>& b, int i, int j) {
+        if (a[i] > b[j]) {
+            swap(a[i], b[j]);
+        }
+    }
+
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+
+            int left = 0;
+            int right = left + gap;
+
+            while (right < len) {
+
+                
+                if (left < m && right < m) {
+                    swapIfGreater(nums1, nums1, left, right);
+                }
+
+               
+                else if (left < m && right >= m) {
+                    swapIfGreater(nums1, nums2, left, right - m);
+                }
+
+                
+                else {
+                    swapIfGreater(nums2, nums2, left - m, right - m);
+                }
+
+                left++;
+                right++;
+            }
+
+            if (gap == 1)
+                break;
+
+            gap = (gap / 2) + (gap % 2);
+        }
+
+        // Put nums2 into the empty positions of nums1
+        for (int i = 0; i < n; i++) {
+            nums1[m + i] = nums2[i];
+        }
+    }
+};
